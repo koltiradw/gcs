d70318dcdab7e5a16193ea1629772f0189f79f39
Ivan Kapranov <koltiradw@yandex.ru>
2024-09-10 01:19:49 +0300

    update lcov gen

--- gcs.go
+++ gcs.go
@@ -1,49 +1,48 @@
 package gcs
 
 import (
-        "os"
-        "log"
-        "runtime/coverage"
-        "net"
-	"os/exec"
-	"fmt"
-	"path/filepath"
-	"io/ioutil"
+	"bufio"
+	"bytes"
 	"encoding/binary"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime/coverage"
+	"os/signal"
+	"context"
 )
 
 const HOST = "localhost"
 const TYPE = "tcp"
 const PORT = "3001"
 
-
 const PROF_FILE = "coverage.profile"
-const LCOV_FILE = "coverage.lcov"
 
 // WuppieFuzz proto for lcov coverage client
 const HEADER_SIZE = 8
+
 var REQUEST_HEADER = [...]byte{0x01, 0xC0, 0xC0, 0x10, 0x07}
+
 const BLOCK_CMD_DUMP = 0x40
+
 var COVERAGE_INFO_RESPONSE = []byte{0x11}
 var CMD_OK_RESPONSE = []byte{0x20}
 
 func removeGlob(path string) (err error) {
 	contents, err := filepath.Glob(path)
 	if err != nil {
-        	return
+		return
 	}
-    	for _, item := range contents {
-        	err = os.RemoveAll(item)
-        	if err != nil {
-            	return
-        	}
-    	}
-    	return
-}    
-
-func dumpCoverage() {
-	coverage.WriteCountersDir(os.Getenv("GOCOVERDIR"))
-        //coverage.ClearCounters()
+	for _, item := range contents {
+		err = os.RemoveAll(item)
+		if err != nil {
+			return
+		}
+	}
+	return
 }
 
 func getLCOV() []byte {
@@ -51,46 +50,44 @@ func getLCOV() []byte {
 	cover_dir := os.Getenv("GOCOVERDIR")
 	path_to_proffile := filepath.Join(cover_dir, PROF_FILE)
 	cmd := exec.Command("go", "tool", "covdata", "textfmt", fmt.Sprintf("-i=%s", cover_dir), fmt.Sprintf("-o=%s", path_to_proffile))
-	
+
 	_, err := cmd.Output()
-	if err != nil {
-  		log.Fatal(err)
-	}
-
-
-	//gen lcov file
-	path_to_lcov_file := filepath.Join(cover_dir, LCOV_FILE)
-	cmd = exec.Command("gcov2lcov", fmt.Sprintf("-infile=%s", path_to_proffile), fmt.Sprintf("-outfile=%s", path_to_lcov_file))	
-	
-	_, err = cmd.Output()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-
-	content, err := ioutil.ReadFile(path_to_lcov_file)
+	prof_file, err := os.Open(path_to_proffile)
 
 	if err != nil {
-                log.Fatal(err)
-        }
-	
+		log.Fatal(err)
+	}
+
+	defer prof_file.Close()
+
+	prof_reader := bufio.NewReader(prof_file)
+
+	lcov_buffer := bytes.NewBuffer(nil)
+	writer := bufio.NewWriter(lcov_buffer)
+
+	ConvertCoverage(prof_reader, writer)
+
 	removeGlob(fmt.Sprintf("%s/covcounters*", cover_dir))
 
-	return content
+	return lcov_buffer.Bytes()
 
 }
 
-func handleRequest(conn net.Conn){
+func handleRequest(_ context.Context, conn net.Conn) {
 	buffer := make([]byte, 8)
 	_, err := conn.Read(buffer)
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	cmd := buffer[5]
 
 	if int(cmd) == BLOCK_CMD_DUMP {
-		dumpCoverage()
+		coverage.WriteCountersDir(os.Getenv("GOCOVERDIR"))
 		lcov := getLCOV()
 		size := make([]byte, 4)
 		binary.LittleEndian.PutUint32(size, uint32(len(lcov)))
@@ -104,7 +101,7 @@ func handleRequest(conn net.Conn){
 	if int(reset_byte) != 0 {
 		coverage.ClearCounters()
 	}
-	
+
 	conn.Write(CMD_OK_RESPONSE)
 }
 
@@ -113,21 +110,22 @@ func init() {
 }
 
 func startCoverageServer() {
-	listen, err := net.Listen(TYPE, HOST + ":" + PORT)
+	ctx := context.Background()
+	ctx, _ = signal.NotifyContext(ctx, os.Interrupt, os.Kill)
+
+	lc := new(net.ListenConfig)
+	listen, err := lc.Listen(ctx, TYPE, HOST+ ":" +PORT)
 	if err != nil {
 		log.Fatal(err)
-		os.Exit(1)
 	}
-
 	defer listen.Close()
 
 	conn, err := listen.Accept()
 	if err != nil {
 		log.Fatal(err)
-		os.Exit(1)
 	}
 
-	for {
-		handleRequest(conn)
+	for ctx.Err() == nil {
+		handleRequest(ctx, conn)
 	}
 }
